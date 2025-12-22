a2d90deabe638f17cacae2b427c577f27d274d97
kostasboubouk <kostas.bouboukas@icloud.com>
2025-12-22 12:55:01 +0200

    Updates on nvim, i3 and picom

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,60 +1,60 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "eslint", "pyright", "sqlls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "eslint", "pyright", "sqlls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Correct way to set up language servers using lspconfig
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-      })
+			-- Correct way to set up language servers using lspconfig
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("ts_ls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("eslint", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("eslint", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("pyright", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("pyright", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("sqlls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("sqlls", {
+				capabilities = capabilities,
+			})
 
-      -- The commented out examples were correct syntax:
-      -- lspconfig.phpactor.setup({
-      --   capabilities = capabilities,
-      -- })
+			-- The commented out examples were correct syntax:
+			-- lspconfig.phpactor.setup({
+			--   capabilities = capabilities,
+			-- })
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,34 @@
-  -- Treesitter
-  return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'ruby', 'javascript', 'typescript', 'html', 'css' },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+-- Treesitter
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		require("nvim-treesitter").install({
+			"c",
+			"ecma",
+			"jsx",
+			"python",
+			"bash",
+			"zsh",
+			"html_tags",
+			"rust",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"ruby",
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+		})
+	end,
+}
