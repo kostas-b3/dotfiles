e7da2b3623ad96db38b1e26f1e5aee0047bd04a4
kostasboubouk <kostas.bouboukas@icloud.com>
2025-12-27 01:03:10 +0200

    updates

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,32 +1,33 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {},
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        rust = { "rustfmt" },
-        javascript = { "prettier", "prettierd" },
-        sql = { "sqlfmt" },
-        php = { "pretty-php" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 5000,
-      },
-    })
+	opts = {},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    vim.keymap.set({ "n", "v" }, "<leader>ll", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 5000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
+				javascript = { "prettier", "prettierd" },
+				sql = { "sqlfmt" },
+				php = { "pretty-php" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 5000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>ll", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 5000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
